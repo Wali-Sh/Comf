abfcd3fba89f732e8854bfef7d4cbdccd7956c0b
Shokrullah Wali <211773@astanait.edu.kz>
2022-05-22 14:41:53 +0600

    new

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ const subscribers = require('./routs/subscribers')
 const adminRout = require('./routs/admin.router')
 
 app.set('view-engine', 'ejs');
+/*
 app.use(flash())
 app.use(session({ 
     secret: process.env.SESSION_SECRET,
@@ -35,6 +36,7 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(methodOverride('_method'))
+*/
 
 // database connection
 mongodb.connect(process.env.DATABASE_URL,{
